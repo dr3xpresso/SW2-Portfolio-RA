0314dda96f680fb9b0dfc8a21697998ad975ed74
Robert Ashley <rashley6957@gmail.com>
2025-12-10 20:14:43 -0500

    Update Playlist1Test.java

--- test/Playlist1Test.java
+++ test/Playlist1Test.java
@@ -1,3 +1,17 @@
+import components.playlist.Playlist1;
+
+/**
+ * Customized JUnit test fixture for {@code Playlist1}.
+ */
 public class Playlist1Test extends PlaylistTest {
 
+    @Override
+    protected final Playlist constructorTest() {
+        return new Playlist1();
+    }
+
+    @Override
+    protected final Playlist constructorRef() {
+        return new Playlist1();
+    }
 }
